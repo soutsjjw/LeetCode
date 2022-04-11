2e101fe743fe285731fc0bd4d678f18c6c163138
soutsjjw <souts.jjw@gmail.com>
2022-04-11 17:30:47 +0800

    9. Palindrome Number

---
+++ LeetCode.UnitTest/9_Palindrome_NumberTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode.UnitTest
+{
+    public class _9_SolutionTests
+    {
+        _9_Solution solution;
+
+        [SetUp]
+        public void Setup()
+        {
+            solution = new _9_Solution();
+        }
+
+        [TestCase(121)]
+        [TestCase(0)]
+        [TestCase(11)]
+        [TestCase(1001)]
+        public void True_Test(int x)
+        {
+            // Act & Assert
+            Assert.IsTrue(solution.IsPalindrome(x));
+        }
+
+        [TestCase(-121)]
+        [TestCase(10)]
+        [TestCase(100)]
+        public void False_Test(int x)
+        {
+            // Act & Assert
+            Assert.IsFalse(solution.IsPalindrome(x));
+        }
+    }
+}
---
+++ LeetCode/9_Palindrome_Number.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode
+{
+    /// <summary>
+    /// https://leetcode.com/problems/palindrome-number/
+    /// </summary>
+    public class _9_Solution
+    {
+        public bool IsPalindrome(int x)
+        {
+            if (x < 0)
+                return false;
+
+            var str = x.ToString();
+            var start = 0;
+            var end = str.Length - 1;
+            for (; ; )
+            {
+                Console.WriteLine($"start: {start}, end: {end}");
+                if (str[start] != str[end])
+                    return false;
+
+                if (start == end || start > end)
+                    return true;
+
+                start += 1;
+                end -= 1;
+            }
+        }
+    }
+}
