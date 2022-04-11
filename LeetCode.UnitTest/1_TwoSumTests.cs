bb5712ee73f289f88d864fce233836b0f5925db2
soutsjjw <souts.jjw@gmail.com>
2022-04-11 15:50:39 +0800

    1. Two Sum

---
+++ LeetCode.UnitTest/1_TwoSumTests.cs
@@ -0,0 +1,83 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode.UnitTest
+{
+    public class _1_SolutionTests
+    {
+        _1_Solution solution;
+
+        [SetUp]
+        public void Setup()
+        {
+            solution = new _1_Solution();
+        }
+
+        [Test]
+        public void Example1Test()
+        {
+            // Arrange
+            var nums = new int[] { 2, 7, 11, 15 };
+            var target = 9;
+
+            // Act
+            var result = solution.TwoSum(nums, target);
+
+            // Assert
+            Assert.AreEqual(2, result.Length);
+            Assert.AreEqual(0, result[0]);
+            Assert.AreEqual(1, result[1]);
+            Assert.AreEqual(target, nums[result[0]] + nums[result[1]]);
+        }
+
+        [Test]
+        public void Example2Test()
+        {
+            // Arrange
+            var nums = new int[] { 3, 2, 4 };
+            var target = 6;
+
+            // Act
+            var result = solution.TwoSum(nums, target);
+
+            // Assert
+            Assert.AreEqual(2, result.Length);
+            Assert.AreEqual(1, result[0]);
+            Assert.AreEqual(2, result[1]);
+            Assert.AreEqual(target, nums[result[0]] + nums[result[1]]);
+        }
+
+        [Test]
+        public void Example3Test()
+        {
+            // Arrange
+            var nums = new int[] { 3, 3 };
+            var target = 6;
+
+            // Act
+            var result = solution.TwoSum(nums, target);
+
+            // Assert
+            Assert.AreEqual(2, result.Length);
+            Assert.AreEqual(0, result[0]);
+            Assert.AreEqual(1, result[1]);
+            Assert.AreEqual(target, nums[result[0]] + nums[result[1]]);
+        }
+
+        [Test]
+        public void Example4Test()
+        {
+            // Arrange
+            var nums = new int[] { 2, 5, 5, 11 };
+            var target = 10;
+
+            // Act
+            var result = solution.TwoSum(nums, target);
+
+            // Assert
+            Assert.AreEqual(2, result.Length);
+            Assert.AreEqual(1, result[0]);
+            Assert.AreEqual(2, result[1]);
+            Assert.AreEqual(target, nums[result[0]] + nums[result[1]]);
+        }
+    }
+}
---
+++ LeetCode/1_TwoSum.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode
+{
+    /// <summary>
+    /// https://leetcode.com/problems/two-sum/
+    /// </summary>
+    public class _1_Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> dicValue = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var diff = target - nums[i];
+                if (dicValue.ContainsKey(diff))
+                {
+                    return new int[] { dicValue[diff], i };
+                }
+
+                dicValue[nums[i]] = i;
+            }
+
+            throw new ArgumentOutOfRangeException();
+        }
+    }
+}
