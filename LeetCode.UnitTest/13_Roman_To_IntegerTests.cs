db5eb16d03c8d0db1f138018069ec5abca425b5a
soutsjjw <souts.jjw@gmail.com>
2022-04-11 18:14:18 +0800

    13. Roman to Integer

---
+++ LeetCode.UnitTest/13_Roman_To_IntegerTests.cs
@@ -0,0 +1,67 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode.UnitTest
+{
+    public class _13_SolutionTests
+    {
+        _13_Solution solution;
+
+        [SetUp]
+        public void Setup()
+        {
+            solution = new _13_Solution();
+        }
+
+        [Test]
+        public void Three_Test()
+        {
+            // Arrange
+            var s = "III";
+
+            // Act
+            var result = solution.RomanToInt(s);
+
+            // Assert
+            Assert.AreEqual(3, result);
+        }
+
+        [Test]
+        public void Fifty_Eight_Test()
+        {
+            // Arrange
+            var s = "LVIII";
+
+            // Act
+            var result = solution.RomanToInt(s);
+
+            // Assert
+            Assert.AreEqual(58, result);
+        }
+
+        [Test]
+        public void One_Thousand_Nine_Hundred_Ninety_Four_Test()
+        {
+            // Arrange
+            var s = "MCMXCIV";
+
+            // Act
+            var result = solution.RomanToInt(s);
+
+            // Assert
+            Assert.AreEqual(1994, result);
+        }
+
+        [Test]
+        public void One_Thousand_Four_Hundred_Seventy_Six_Test()
+        {
+            // Arrange
+            var s = "MCDLXXVI";
+
+            // Act
+            var result = solution.RomanToInt(s);
+
+            // Assert
+            Assert.AreEqual(1476, result);
+        }
+    }
+}
---
+++ LeetCode/13_Roman_To_Integer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode
+{
+    /// <summary>
+    /// https://leetcode.com/problems/roman-to-integer/
+    /// </summary>
+    public class _13_Solution
+    {
+        Dictionary<char, int> dicValues = new Dictionary<char, int>()
+        {
+            { 'I', 1 },
+            { 'V', 5 },
+            { 'X', 10 },
+            { 'L', 50 },
+            { 'C', 100 },
+            { 'D', 500 },
+            { 'M', 1000 },
+        };
+
+        public int RomanToInt(string s)
+        {
+            var chars = s.ToCharArray();
+            int value = 0, curr = 0, prev = 0;
+            for (var i = chars.Length - 1; i >= 0; i--)
+            {
+                curr = dicValues[chars[i]];
+
+                if (prev > curr)
+                {
+                    value -= curr;
+                }
+                else
+                {
+                    value += curr;
+                }
+
+                prev = curr;
+            }
+
+            return value;
+        }
+    }
+}
