2dbcf2c592bfadb950871a6922d36145fce9df9a
souts jjw <souts.jjw@gmail.com>
2022-04-15 14:59:49 +0800

    20. Valid Parentheses

---
+++ LeetCode.UnitTest/20_ValidParentheses.cs
@@ -0,0 +1,106 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode.UnitTest
+{
+    public class _20_SolutionTests
+    {
+        _20_Solution solution;
+
+        [SetUp]
+        public void Setup()
+        {
+            solution = new _20_Solution();
+        }
+
+        [Test]
+        public void Example1Test()
+        {
+            // Arrange
+            var s = "()";
+
+            // Act
+            var result = solution.IsValid(s);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void Example2Test()
+        {
+            // Arrange
+            var s = "()[]{}";
+
+            // Act
+            var result = solution.IsValid(s);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void Example3Test()
+        {
+            // Arrange
+            var s = "(]";
+
+            // Act
+            var result = solution.IsValid(s);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void Example4Test()
+        {
+            // Arrange
+            var s = "([{}])";
+
+            // Act
+            var result = solution.IsValid(s);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void Example5Test()
+        {
+            // Arrange
+            var s = "([{)}]";
+
+            // Act
+            var result = solution.IsValid(s);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void Example6Test()
+        {
+            // Arrange
+            var s = "]";
+
+            // Act
+            var result = solution.IsValid(s);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void Example7Test()
+        {
+            // Arrange
+            var s = "[[[]";
+
+            // Act
+            var result = solution.IsValid(s);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+    }
+}
---
+++ LeetCode/20_ValidParentheses.cs
@@ -0,0 +1,41 @@
+﻿namespace LeetCode
+{
+	/// <summary>
+	/// https://leetcode.com/problems/valid-parentheses/submissions/
+	/// </summary>
+	public class _20_Solution
+    {
+        public bool IsValid(string s)
+		{
+			var stack = new Stack<char>();
+
+			foreach (char c in s)
+			{
+				if (c == '(')
+				{
+					stack.Push(')');
+					continue;
+				}
+
+				if (c == '[')
+				{
+					stack.Push(']');
+					continue;
+				}
+
+				if (c == '{')
+				{
+					stack.Push('}');
+					continue;
+				}
+
+				if (stack.Count == 0 || stack.Pop() != c)
+				{
+					return false;
+				}
+			}
+
+			return !stack.Any();
+		}
+    }
+}
