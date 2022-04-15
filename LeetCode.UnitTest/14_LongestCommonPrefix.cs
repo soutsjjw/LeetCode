edf2ce6a3135dd20a07eb775107bc1198760359a
souts jjw <souts.jjw@gmail.com>
2022-04-15 13:42:27 +0800

    14. Longest Common Prefix

---
+++ LeetCode.UnitTest/14_LongestCommonPrefix.cs
@@ -0,0 +1,67 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode.UnitTest
+{
+    public class _14_SolutionTests
+    {
+        _14_Solution solution;
+
+        [SetUp]
+        public void Setup()
+        {
+            solution = new _14_Solution();
+        }
+
+        [Test]
+        public void Example1Test()
+        {
+            // Arrange
+            var strs = new string[] { "flower", "flow", "flight" };
+
+            // Act
+            var result = solution.LongestCommonPrefix(strs);
+
+            // Assert
+            Assert.AreEqual("fl", result);
+        }
+
+        [Test]
+        public void Example2Test()
+        {
+            // Arrange
+            var strs = new string[] { "dog", "racecar", "car" };
+
+            // Act
+            var result = solution.LongestCommonPrefix(strs);
+
+            // Assert
+            Assert.AreEqual("", result);
+        }
+
+        [Test]
+        public void Example3Test()
+        {
+            // Arrange
+            var strs = new string[] { "a" };
+
+            // Act
+            var result = solution.LongestCommonPrefix(strs);
+
+            // Assert
+            Assert.AreEqual("a", result);
+        }
+
+        [Test]
+        public void Example4Test()
+        {
+            // Arrange
+            var strs = new string[] { "ab", "a" };
+
+            // Act
+            var result = solution.LongestCommonPrefix(strs);
+
+            // Assert
+            Assert.AreEqual("a", result);
+        }
+    }
+}
---
+++ LeetCode/14_LongestCommonPrefix.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode
+{
+    /// <summary>
+    /// https://leetcode.com/problems/longest-common-prefix/
+    /// </summary>
+    public class _14_Solution
+    {
+        public string LongestCommonPrefix(string[] strs)
+		{
+			if (strs.Length == 1)
+				return strs[0];
+
+			strs = strs.ToList().OrderBy(x => x.Length).ToArray();
+			var arrayLength = strs.Length;
+			var shortestStringLength = strs[0].Length;
+			var result = "";
+
+			for (int i = 1; i <= shortestStringLength; i++)
+			{
+				result = strs[0].Substring(0, i);
+				for (int j = 1; j < arrayLength; j++)
+				{
+					if (!strs[j].StartsWith(result))
+					{
+						return result.Substring(0, result.Length - 1);
+					}
+				}
+			}
+
+			return strs[0];
+		}
+    }
+}
